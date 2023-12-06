d6a02df1c11bc3c94fe53d05f6ed33363d3ff08c
dimas <dimasibras10@gmail.com>
2023-12-06 13:20:03 +0700

    Initial commit

---
+++ src/PrimitiveDataType.java
@@ -0,0 +1,23 @@
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        String name = "Mr. Bean";
+        short idNumber = 123;
+        int age = 44;
+        boolean marriedStatus = false;
+        char bloodType = 'O';
+        double height = 165.55f;
+        float weight = 65.5f;
+        byte countryPhoneCode = 62;
+        long phoneNumber = 1234567890l;
+
+        System.out.println("Name                = "+name);
+        System.out.println("ID Number           = "+idNumber);
+        System.out.println("Age                 = "+age);
+        System.out.println("Married             = "+marriedStatus);
+        System.out.println("Blood Type          = "+bloodType);
+        System.out.println("Height              = "+height);
+        System.out.println("Weight              = "+weight);
+        System.out.println("Country Phone Code  = +"+countryPhoneCode);
+        System.out.println("Phone Number        = "+phoneNumber);
+    }
+}
